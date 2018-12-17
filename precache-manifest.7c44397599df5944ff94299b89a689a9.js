1b185806468ec8d13775722b3820e9d3e7660576
ZanePartain <zane.partain@wsu.edu>
2018-12-17 12:54:43 -0800

    Updates

---
+++ precache-manifest.7c44397599df5944ff94299b89a689a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "364bf3d3d2dfcf25b40d",
+    "url": "/static/js/main.364bf3d3.chunk.js"
+  },
+  {
+    "revision": "324b6cf3b56bf43378ce",
+    "url": "/static/js/1.324b6cf3.chunk.js"
+  },
+  {
+    "revision": "364bf3d3d2dfcf25b40d",
+    "url": "/static/css/main.6a6e3d25.chunk.css"
+  },
+  {
+    "revision": "eff3b788283331f36efc8db1575b2288",
+    "url": "/index.html"
+  }
+];
