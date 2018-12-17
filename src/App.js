c5083dbae65131836f50628745ea4dc63deb0c53
ZanePartain <zane.partain@wsu.edu>
2018-12-17 13:00:04 -0800

    init

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <p>
-           ZANE PARTAIN
+           ZANE PARTAIN!!
           </p>
         </header>
       </div>
